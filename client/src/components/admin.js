63aa684c61471fe93974dd53178ce5a73870c920
kiet5454123 <57711477+kiet5454123@users.noreply.github.com>
2023-04-15 12:28:10 +0700

    Update admin.js

--- client/src/components/admin.js
+++ client/src/components/admin.js
@@ -76,7 +76,7 @@ class Admin extends Component{
   }
   componentDidMount() {
     this.loadBlockchainData()
-    fetch('http://localhost:3000/admin', {
+    fetch('https://iuh-bank-server.onrender.com/admin', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
